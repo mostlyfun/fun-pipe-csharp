e997a2114a7b2dead21b335634a25530b46b50fb
Ugur Arikan <ugur.arikan@dpdhl.com>
2022-04-08 20:40:10 +0200

    Extensions siplit into partical classes

--- src/Fun.Pipe/Fun.Pipe/ExtensionsOpt.cs
+++ src/Fun.Pipe/Fun.Pipe/ExtensionsOpt.cs
@@ -79,33 +79,9 @@ public static partial class Extensions
     /// Returns itself.
     /// </summary>
     public static Opt<T> RunIfNone<T>(this Opt<T> maybe, Action action) { if (maybe.IsNone) action(); return maybe; }
-    /// <summary>
-    /// Maps <paramref name="maybe"/> into <paramref name="some"/>(maybe.Unwrap()) whenever maybe.IsSome; and into <paramref name="none"/>() otherwise.
-    /// </summary>
-    public static TOut Match<T, TOut>(this Opt<T> maybe, Func<T, TOut> some, Func<TOut> none)
-    {
-        if (maybe.IsSome) return some(maybe.value);
-        else return none();
-    }
-    /// <summary>
-    /// Maps <paramref name="maybe"/> into <paramref name="some"/>(maybe.Unwrap()) whenever maybe.IsSome; and into <paramref name="none"/> otherwise.
-    /// </summary>
-    public static TOut Match<T, TOut>(this Opt<T> maybe, Func<T, TOut> some, TOut none)
-    {
-        if (maybe.IsSome) return some(maybe.value);
-        else return none;
-    }
-
+    
 
     // Run
-    /// <summary>
-    /// Runs <paramref name="action"/>, and returns back <paramref name="value"/>.
-    /// </summary>
-    public static T Run<T>(this T value, Action action) { action(); return value; }
-    /// <summary>
-    /// Runs <paramref name="action"/>(<paramref name="value"/>), and returns back <paramref name="value"/>.
-    /// </summary>
-    public static T Run<T>(this T value, Action<T> action) { action(value); return value; }
     /// <summary>
     /// Runs <paramref name="action"/>() only if maybe.IsSome, and returns back <paramref name="maybe"/>.
     /// </summary>
@@ -114,23 +90,6 @@ public static partial class Extensions
     /// Runs <paramref name="action"/>(<paramref name="maybe"/>.Unwrap()) only if maybe.IsSome, and returns back <paramref name="maybe"/>.
     /// </summary>
     public static Opt<T> Run<T>(this Opt<T> maybe, Action<T> action) { if (maybe.IsSome) action(maybe.value); return maybe; }
-
-
-    // Map: ()->T
-    /// <summary>
-    /// Returns <paramref name="map"/>().
-    /// </summary>
-    public static TOut Map<TOut>(Func<TOut> map) => map();
-
-    // Map: t->T
-    /// <summary>
-    /// Returns <paramref name="map"/>().
-    /// </summary>
-    public static TOut Map<T, TOut>(this T value, Func<TOut> map) => map();
-    /// <summary>
-    /// Returns <paramref name="map"/>(<paramref name="value"/>).
-    /// </summary>
-    public static TOut Map<T, TOut>(this T value, Func<T, TOut> map) => map(value);
 
 
     // Map: Opt<t>->Opt<T>
@@ -301,12 +260,6 @@ public static partial class Extensions
 
     // Collections
     /// <summary>
-    /// Creates and returns and IEnumerable yielding unwrapped values of elements that are <see cref="Opt{T}.IsSome"/>.
-    /// </summary>
-    /// <typeparam name="T">Underlying type of the Opt.</typeparam>
-    /// <param name="collection">Collection of opt values.</param>
-    public static IEnumerable<T> UnwrapValues<T>(this IEnumerable<Opt<T>> collection) => collection.Where(x => x.IsSome).Select(x => x.Unwrap());
-    /// <summary>
     /// Opt counterpart of FirstOrDefault, which returns the first non-null element if <paramref name="enumerable"/> has any, None otherwise.
     /// </summary>
     public static Opt<T> FirstOrNone<T>(this IEnumerable<T> enumerable)
@@ -343,12 +296,6 @@ public static partial class Extensions
     /// Opt counterpart of LastOrDefault, which returns the last Some element if <paramref name="enumerable"/> has any, None otherwise.
     /// </summary>
     public static Opt<T> LastOrNone<T>(this IEnumerable<Opt<T>> enumerable) => FirstOrNone(enumerable.Reverse());
-    /// <summary>
-    /// Creates and returns and IEnumerable yielding unwrapped values of elements that are <see cref="Res{T}.IsOk"/>.
-    /// </summary>
-    /// <typeparam name="T">Underlying type of the Res.</typeparam>
-    /// <param name="collection">Collection of result values.</param>
-    public static IEnumerable<T> UnwrapValues<T>(this IEnumerable<Res<T>> collection) => collection.Where(x => x.IsOk).Select(x => x.Unwrap());
     /// <summary>
     /// Opt counterpart of FirstOrDefault over results collection, which returns the first Ok element of <paramref name="enumerable"/> if any, None otherwise.
     /// </summary>
---
+++ src/Fun.Pipe/Fun.Pipe/ExtensionsT.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace Fun;
+
+/// <summary>
+/// Static extension or utility methods for Opt, Res, or Pipe.
+/// </summary>
+public static partial class Extensions
+{
+    // Opt - None
+    /// <summary>
+    /// Maps <paramref name="maybe"/> into <paramref name="some"/>(maybe.Unwrap()) whenever maybe.IsSome; and into <paramref name="none"/>() otherwise.
+    /// </summary>
+    public static TOut Match<T, TOut>(this Opt<T> maybe, Func<T, TOut> some, Func<TOut> none)
+    {
+        if (maybe.IsSome) return some(maybe.value);
+        else return none();
+    }
+    /// <summary>
+    /// Maps <paramref name="maybe"/> into <paramref name="some"/>(maybe.Unwrap()) whenever maybe.IsSome; and into <paramref name="none"/> otherwise.
+    /// </summary>
+    public static TOut Match<T, TOut>(this Opt<T> maybe, Func<T, TOut> some, TOut none)
+    {
+        if (maybe.IsSome) return some(maybe.value);
+        else return none;
+    }
+
+
+    // Run
+    /// <summary>
+    /// Runs <paramref name="action"/>, and returns back <paramref name="value"/>.
+    /// </summary>
+    public static T Run<T>(this T value, Action action) { action(); return value; }
+    /// <summary>
+    /// Runs <paramref name="action"/>(<paramref name="value"/>), and returns back <paramref name="value"/>.
+    /// </summary>
+    public static T Run<T>(this T value, Action<T> action) { action(value); return value; }
+
+
+    // Map: ()->T
+    /// <summary>
+    /// Returns <paramref name="map"/>().
+    /// </summary>
+    public static TOut Map<TOut>(Func<TOut> map) => map();
+
+    // Map: t->T
+    /// <summary>
+    /// Returns <paramref name="map"/>().
+    /// </summary>
+    public static TOut Map<T, TOut>(this T value, Func<TOut> map) => map();
+    /// <summary>
+    /// Returns <paramref name="map"/>(<paramref name="value"/>).
+    /// </summary>
+    public static TOut Map<T, TOut>(this T value, Func<T, TOut> map) => map(value);
+
+
+    // Collections
+    /// <summary>
+    /// Creates and returns and IEnumerable yielding unwrapped values of elements that are <see cref="Opt{T}.IsSome"/>.
+    /// </summary>
+    /// <typeparam name="T">Underlying type of the Opt.</typeparam>
+    /// <param name="collection">Collection of opt values.</param>
+    public static IEnumerable<T> UnwrapValues<T>(this IEnumerable<Opt<T>> collection) => collection.Where(x => x.IsSome).Select(x => x.Unwrap());
+    /// <summary>
+    /// Creates and returns and IEnumerable yielding unwrapped values of elements that are <see cref="Res{T}.IsOk"/>.
+    /// </summary>
+    /// <typeparam name="T">Underlying type of the Res.</typeparam>
+    /// <param name="collection">Collection of result values.</param>
+    public static IEnumerable<T> UnwrapValues<T>(this IEnumerable<Res<T>> collection) => collection.Where(x => x.IsOk).Select(x => x.Unwrap());
+}
